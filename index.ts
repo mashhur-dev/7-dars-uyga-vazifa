528e2e6ce2a5f6b034af2505ad4ef8a0639b982d
mashhurbek <mashhurtadjiyev@gmail.com>
2022-11-20 04:06:09 +0500

    7-dars uyga vazifa

---
+++ index.ts
@@ -0,0 +1,182 @@
+// 1-vazifa
+
+
+// type Add = (a: string, b: number) => string
+
+// function add(a: string, b: number) {
+//     return a + b
+// }
+
+// function addTwo(name: string, age: number): string {
+//     return name + " " + age + " years old"
+// }
+
+// const plus: Add = addTwo
+
+// let result: string = plus("Mashhurbek", 20)
+
+// console.log(result);
+
+// *******************************************************************
+
+
+// 2-vazifa
+
+// type persons = (firstname: string, lastname: string) => string
+
+// function person (firstname: string, lastname: string) {
+//     return 2
+// }
+
+// function personTwo(firstname: string, lastname: string): string {
+//     return "Ism: " + firstname + "\n" + "Familiya: " + lastname
+// }
+
+// const plus: persons = personTwo
+
+// let natija: string = plus("Mashhurbek", "Tadjiyev")
+
+// console.log(natija);
+
+// *******************************************************************
+
+
+// 3-vazifa
+
+// type killProcessed = (app: string) => string
+
+// function killProcess (app: string) {
+//     return "process killed"
+// }
+
+// function kill(app: string): string {
+//     return "process killed"
+// }
+
+// const fulus: killProcessed = kill
+
+// let result: string = fulus("process killed")
+
+// console.log(result);
+
+// *******************************************************************
+
+
+// 4-vazifa
+
+// type clones = (clone: string) => string
+
+// function clone() {
+//     return "cloden"
+// }
+
+// function cloneTwo(clone: string): string {
+//     return "cloden"
+// }
+
+// const hmm: clones = cloneTwo
+
+// let result: string = hmm("cloden")
+
+// console.log(result);
+
+
+// *******************************************************************
+
+
+
+
+// Object
+
+
+// 1-vazifa 
+
+// const user = {
+//     ism: "Mashhurbek",
+//     familiya: "Tadjiyev",
+//     yosh: 20
+// }
+
+// type Odom = {
+//     ism: string,
+//     familiya: string,
+//     yosh: number
+// }
+
+// const mashhur: Odom = {
+//     ism: "Mashhurbek",
+//     familiya: "Tadjiyev",
+//     yosh: 20
+// }
+
+// console.log(mashhur);
+
+// *******************************************************************
+
+
+// 2-vazifa
+
+// const point = {
+//     x: 15,
+//     y: 65
+// }
+// type points = {
+//     x: number,
+//     y: number,
+// }
+
+// const axa: points = {
+//     x: 15,
+//     y: 65
+// }
+
+// console.log(axa);
+
+
+// *******************************************************************
+
+
+// 3-vazifa
+
+// const app = {
+//     name: "Flappy Bird",
+//     downloads: 1456,
+//     description: "Best game in the World!"
+// }
+
+
+// type apps = {
+//     name: string,
+//     downloads: number,
+//     description: string
+// }
+
+// const appTwo: apps = {
+//     name: "Flappy Bird",
+//     downloads: 1456,
+//     description: "Best game in the World!"
+// }
+
+// console.log(appTwo);
+
+// *******************************************************************
+
+
+// 4-vazifa
+
+// const answerlist = {
+//     savol: 'Length of lake Niagara?',
+//     javob: 123564
+// }
+
+// type answer = {
+//     savol: string,
+//     javob: number,
+// }
+
+// const list: answer = {
+//     savol: 'Length of lake Niagara?',
+//     javob: 123564,
+// }
+
+// console.log(list);
